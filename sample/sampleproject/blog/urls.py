2b7d02aab116f0a8227d592fa8a0cf8617ff94f7
hossein <hossein.sharifi@crowncapital.nz>
2022-11-11 11:13:25 +0330

    add code on class

--- sample/sampleproject/blog/urls.py
+++ sample/sampleproject/blog/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
 from django.views.generic import TemplateView
 from . import views
-# app_name = 'blog'
+app_name = 'blog'
 urlpatterns = [
     #simple view
-    # path('first',views.first_view),
+    path('first',views.first_view),
     path('',views.PostsView.as_view(),name='main-page'),
     path('list',views.PostListView.as_view(),name='list-page'),
-
-
     path('post/<int:pk>',views.PostDetailView.as_view(),name='single-post'),
     # path('post-detail/<int:pk>',views.PostDetailView.as_view(),name='single-post'),
 
--- sample/sampleproject/blog/views.py
+++ sample/sampleproject/blog/views.py
@@ -4,65 +4,77 @@ from django.views import View
 from django.http import Http404
 
 
-from .models import Post
+from .models import Post,Category
 
 # request : https://docs.djangoproject.com/en/4.1/ref/request-response/
 
 def first_view(request):
-    return render(request,'single-post.html',{})
+    return render(request,'index.html',{})
 
 
 
 class PostsView(View):
 
     def get(self,request,*args, **kwargs):
-
         # get list of posts
+        # posts = Post.objects.filter(title__startswith='s')
         posts = Post.objects.all()
-        context = {'posts':posts}
+        category = Category.objects.all()
+        context = {'post':posts,'category':category}
         return render(request,'main.html',context)
+    
 
 
-class PostDetailView(View):
 
-    def get(self,request,*args, **kwargs):
-        # get one post
-        pk = kwargs['pk']
-        print(kwargs)
-        try:    
-            post = Post.objects.get(pk=pk)  # or
-        except Post.DoesNotExist:
-            # post = {}
-            raise Http404("No MyModel matches the given query.")
-        # post = get_object_or_404(Post,pk=pk)
+# class PostDetailView(View):
 
-        context = {'post':post}
-        return render(request,'single-post.html',context)
+#     def get(self,request,*args, **kwargs):
+#         # get one post
+#         print(kwargs)
+#         pk = kwargs['pk']
+#         try:
+#             post = Post.objects.get(pk=pk)  # or
+#         except Post.DoesNotExist:
+#             raise Http404("No Post matches the given query.")
+       
+
+#         # post = get_object_or_404(Post,pk=pk)
+
+#         context = {'post':post}
+#         return render(request,'single-post.html',context)
 
 
 # class PostDetailView(DetailView):
 #     model = Post
 
-# class PostDetailView(DetailView):
-#     model = Post
-#     template_name = 'single-post.html'
-#     context_object_name = 'post'
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'single-post.html'
+    context_object_name = 'post'
 
+
+# class PostListView(ListView):
+#     # model = Post
+#     queryset = Post.objects.all()[:2]
+#     context_object_name = 'posts'
+#     template_name = 'main.html'
+    
 
 class PostListView(ListView):
     model = Post
-    
+    # queryset = Post.objects.all()[:2]
+    context_object_name = 'posts'
+    template_name = 'main.html'
 
-# class PostListView(ListView):
-#     model = Post
-#     context_object_name = 'posts'
-#     template_name = 'main.html'
-#     queryset = Post.objects.all()[:2]
 
-#     # def get_context_data(self, **kwargs):
-#     #     context = super().get_context_data(**kwargs)
-#     #     context['now'] = 'now'
-#     #     return context
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cats'] = Category.objects.all()
+        
+        return context
+
+
 
     
 
